ef4bc24b4bcad25e4019fa46591e966dfdff54cb
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-23 21:47:15 -0300

    Exercícios 1 e 2 feitos

---
+++ script.js
@@ -0,0 +1,24 @@
+//Exercício 1
+
+//let numero= Number(prompt("Insira um número par")) 
+//console.log(Number(numero%2))
+
+
+
+//c -> notei que a divisão por número par sempre resta 0
+
+//d -> que a divisão por número ímpar sempre resta 1
+
+
+//Exercício 2:
+
+let numero1=Number(prompt("Escreva o primeiro número"))
+let numero2=Number(prompt("Escreva o segundo número"))
+
+console.log ("O primeiro numero é maior que o segundo?", numero1>numero2)
+
+console.log("O primeiro numero é igual ao segundo?", numero1===numero2);
+
+console.log("O primeiro numero é divisível pelo segundo?", numero1%numero2===0);
+
+console.log("O segundo numero é divisível pelo primeiro?", numero2%numero1===0);
