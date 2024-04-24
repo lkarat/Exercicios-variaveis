cda30bf0f6e0ef6260609e6fd2fd56fab7205dd7
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-23 22:07:17 -0300

    Os 3 exercícios completos

--- script.js
+++ script.js
@@ -12,13 +12,25 @@
 
 //Exercício 2:
 
-let numero1=Number(prompt("Escreva o primeiro número"))
-let numero2=Number(prompt("Escreva o segundo número"))
+//let numero1=Number(prompt("Escreva o primeiro número"))
+//let numero2=Number(prompt("Escreva o segundo número"))
 
-console.log ("O primeiro numero é maior que o segundo?", numero1>numero2)
+//console.log ("O primeiro numero é maior que o segundo?", numero1>numero2)
 
-console.log("O primeiro numero é igual ao segundo?", numero1===numero2);
+//console.log("O primeiro numero é igual ao segundo?", numero1===numero2);
 
-console.log("O primeiro numero é divisível pelo segundo?", numero1%numero2===0);
+//console.log("O primeiro numero é divisível pelo segundo?", numero1%numero2===0);
 
-console.log("O segundo numero é divisível pelo primeiro?", numero2%numero1===0);
+//console.log("O segundo numero é divisível pelo primeiro?", numero2%numero1===0);
+
+// Exercício 3
+
+//a
+console.log ("5 é maior que 20 e também é menor que 2:", 5>20 && 5<2)
+
+console.log("5 é igual a 5 ou é igual a '5':", 5===5 || 5==="5" );
+
+console.log("negação de (vinte é maior que cinquenta):", !(20>50))
+
+console.log ("negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)", 20>50 || 50>60)
+
